c2c0fa88dcde00a6bdf9069d0d111e52c8aa8381
Dzmitry Davidovich <Dzmitrydavidovich89@gmail.com>
2021-04-26 02:45:00 +0300

    add shop page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import './App.css';
-import HomePage from './pages/homepage/homepage.component'
+import { Route, Switch } from 'react-router-dom';
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>hats page</h1>
+    </div>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route  path='/shop' component={ShopPage}/>
+      </Switch>
     </div>
   );
 }
