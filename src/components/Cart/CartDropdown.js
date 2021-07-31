6fe6ccd63e4d158432dd10f02e7c42a416e4fde4
Dzmitry <Dzmitrydavidovich89@gmail.com>
2021-07-31 15:52:33 +0300

    create cartItem component and styling it

--- src/components/Cart/CartDropdown.js
+++ src/components/Cart/CartDropdown.js
@@ -1,17 +1,26 @@
 import React from 'react';
-
+import {connect} from 'react-redux';
 import CustomButton from '../CustomButton/CustomButton';
+import CartItem from '../CartItem/CartItem';
 import './CartDropdown.scss';
 
 
-const CartDropdown = () => {
+const CartDropdown = ({cartItems}) => {
+  console.log(cartItems);
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
+      {cartItems.map((item) => (
+        <CartItem 
+          key={item.id}
+          item={item}/>
+      ))}
       </div>
         <CustomButton inverted>Go to Checkout</CustomButton>
     </div>
   )
 }
-
-export default CartDropdown;
+const mapStateToProps = (state) => (
+  {cartItems: state.cart.cartItems}
+)
+export default connect(mapStateToProps)(CartDropdown);
